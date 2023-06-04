fcc6dd5c8ee9df38fdfff49e5cc639d6e15432a3
Mostafa Mazhar <moustafammazhar@gmail.com>
2023-06-04 09:22:56 -0400

    "added for loop for on each item"

--- src/app.js
+++ src/app.js
@@ -5,5 +5,21 @@ window.onload = function() {
   noun =    ['jogger', 'racoon'];
   extensions = ['.com', '.net', '.us', '.io'];
 
+  // Create an empty array to store the generated domain 
+  domianNames = [];
+  // Iterate over each pronoun, adj, noun, ext
+  for(let pr of pronoun){
+    for(let adjWord of adj){
+      for(let nounWord of noun){
+        for(let ext of extensions){
+       
+        }
+      }
+    }
+  }
+
+
+
+
 };
 
