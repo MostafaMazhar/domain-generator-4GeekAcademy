1bff6a751f7bb9e1006c17148418c7e954da4d95
Mostafa Mazhar <moustafammazhar@gmail.com>
2023-06-04 12:17:40 -0400

    'Added randDomain function'

--- src/app.js
+++ src/app.js
@@ -1,31 +1,38 @@
 window.onload = function() {
  
-  pronoun = ['the', 'our', 'my', 'your', 'his', 'her', 'their'];
-  adj =     ['great', 'big', 'amazing', 'wacky', 'funky', 'quirky', 'zany'];
-  noun =    ['jogger', 'racoon', 'adventure', 'dream', 'wonder', 'bliss', 'magic'];
+  pronoun = ['your', 'his', 'her', 'their', 'the', 'our', 'my'];
+  adj =     ['wacky', 'funky', 'quirky', 'zany', 'great', 'big', 'amazing'];
+  noun =    ['dream', 'wonder', 'bliss', 'magic', 'jogger', 'racoon', 'adventure'];
   extensions = ['.com', '.net', '.us', '.io'];
 
   // Create an empty array to store the generated domain 
-  domainNames = [];
+  let domainNames = [];
   // Iterate over each pronoun, adj, noun, ext
-  for(let pr of pronoun){
-    for(let adjWord of adj){
+  function randDomain(){
+    for(let pr of pronoun){
+     for(let adjWord of adj){
       for(let nounWord of noun){
         for(let ext of extensions){
        let domain = pr + adjWord + nounWord + ext;
       //  Add the domain name to the array of domainNames:
        domainNames.push(domain);
-       console.log(domain);
+       
         }
       }
     }
   }
-   let domainList = document.getElementById('domainList');
-      for(domain of domainNames){
-      let listItems = document.createElement('li');  //  element will represent each domain name in the list
-      listItems.textContent = domain;  // assign domain name as the txt inside the listItem
-      domainList.appendChild(listItems);
-   }
+    return domainNames;
+  };
+
+      allDomainNames = randDomain();
+      console.log(allDomainNames);
+      document.getElementById('domainList').innerHTML = allDomainNames.join('_ <br> _');
   
 };
 
+  //     let domainList = document.getElementById('domainList');
+  //     for(domain of domainNames){
+  //     let listItems = document.createElement('li');  //  element will represent each domain name in the list
+  //     listItems.textContent = domain;  // assign domain name as the txt inside the listItem
+  //     domainList.appendChild(listItems);
+  //     };
