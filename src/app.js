38b5cb122f4615e7f7dc74139c76c608b043bc65
Mostafa Mazhar <moustafammazhar@gmail.com>
2023-06-04 09:45:03 -0400

    'Concatenate the words and ext'

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-window.onload = function() {
+// window.onload = function() {
  
   pronoun = ['the', 'our'];
   adj =     ['great', 'big'];
@@ -6,20 +6,23 @@ window.onload = function() {
   extensions = ['.com', '.net', '.us', '.io'];
 
   // Create an empty array to store the generated domain 
-  domianNames = [];
+  domainNames = [];
   // Iterate over each pronoun, adj, noun, ext
   for(let pr of pronoun){
     for(let adjWord of adj){
       for(let nounWord of noun){
         for(let ext of extensions){
-       
+       let domain = pr + adjWord + nounWord + ext;
+      //  Add the domain name to the array of domainNames:
+       domainNames.push(domain);
+       console.log(domain)
         }
       }
     }
   }
+   
 
 
 
-
-};
+// };
 
